014b376a88318e16ae78810387ac78409ce169a8
Alexandre Duret-Lutz <duret_g@epita.fr>
2000-07-14 09:47:36 +0200

    Point people to the heroes-discuss mailing list and other SourceForge services.

--- src/argv.c
+++ src/argv.c
@@ -93,7 +93,7 @@ print_help (char* argv0)
 	"this:\n\n"
 	"  Options: -gs -d3,buffer=11,count=4\n"
 	"\n"
-	"Report bugs to <duret_g@epita.fr>.");
+	"Report bugs to <heroes-discuss@lists.sourceforge.net>.");
 }
 
 const struct option long_options[] = {
