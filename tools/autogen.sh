f7bfb1b99081a6675f5ecd61ffdf35384880f2e6
Alexandre Duret-Lutz <duret_g@epita.fr>
2000-11-02 12:09:11 +0100

    Remove auto-installed files from CVS. Arrange so most of auto-installed file go in tools/, not in the base directory.
    * COPYING: Delete.
    * INSTALL: Delete.
    * depcomp: Delete.
    * config.guess: Delete.
    * config.sub: Delete.
    * install-sh: Delete.
    * missing: Delete.
    * mkinstalldirs: Delete.
    * doc/texinfo.tex: Delete.
    * doc/mdate-sh: Delete.
    * tools/autogen.sh: New.
    * tools/Makefile.am: New.
    * HACKING: Reflect changes.
    * Makefile.am (SUBDIRS): Add tools.
    
    * acconfig.h: Delete.
    * configure.in: Use AH_TEMPLATE to describe the DEFINEs originaly
    described in acconfig.h

---
+++ tools/autogen.sh
@@ -0,0 +1,75 @@
+#! /bin/sh
+# Copyright 2000  Alexandre Duret-Lutz <duret_g@epita.fr>
+#
+# This file is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+verb=':'
+moreverb=''
+
+function usage ()
+{
+  echo "Usage: autogen.sh [-h] [-v] [-V]"
+}
+
+function die ()
+{
+  echo ${1+"$@"}
+  exit 1
+}
+
+function saferun ()
+{
+  $verb $@
+  $@ || die "*** Error '${1+\"$@\"}' exited with bad status."
+}
+
+while test $# -gt 0 ; do
+  case "${1}" in
+    -h | --h*)
+      usage
+      exit 0 ;;
+    -v | --v*)
+      verb='echo'
+      shift ;;
+    -V | --V*)
+      verb='echo'
+      moreverb='--verbose'
+      shift ;;
+    *)
+      usage
+      exit 1;;
+  esac
+done
+
+test -f configure.in || die "Cannot find configure.in in current directory."
+
+if test -f Makefile.am; then
+  # use the same aclocal flags as in Makefile.am, if specified
+  aclocalflags=`sed -n -e '/ACLOCAL_AMFLAGS/{s/.*=\(.*\)$/\1/
+p
+}' Makefile.am`
+fi
+
+# generate aclocal.m4
+saferun aclocal $aclocalflags ${moreverb}
+
+# generate src/config.h.in
+saferun autoheader ${moreverb}
+
+# generate configure
+saferun autoconf ${moreverb}
+
+# generate **/Makefile.in and add mssing files
+saferun automake --add-missing ${moreverb}
