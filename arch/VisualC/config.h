c56c83e535b72c905c32da7269adb02100bef086
Alexandre Duret-Lutz <duret_g@epita.fr>
2000-12-07 11:58:43 +0100

    * NEWS: Update for 0.7a.

--- arch/VisualC/config.h
+++ arch/VisualC/config.h
@@ -150,7 +150,7 @@
 #define USE_HEROES_DEBUG 1
 
 /* Version number of package */
-#define VERSION "0.7"
+#define VERSION "0.7a"
 
 /* whether byteorder is bigendian */
 /* #undef WORDS_BIGENDIAN */
