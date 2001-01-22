8671cbe6d6a3c512f80bd556396f19e15dc8d9ce
Alexandre Duret-Lutz <duret_g@epita.fr>
2001-01-22 07:52:33 +0000

    * intl/ChangeLog: Update from fileutils-4.0.37. * intl/gettextP.h: Likewise. * lib/hash.c: Likewise. * po/Makefile.in.in: Likewise.

--- intl/gettextP.h
+++ intl/gettextP.h
@@ -44,11 +44,8 @@
 # include <byteswap.h>
 # define SWAP(i) bswap_32 (i)
 #else
-static nls_uint32 SWAP PARAMS ((nls_uint32 i));
-
 static inline nls_uint32
-SWAP (i)
-     nls_uint32 i;
+SWAP (nls_uint32 i)
 {
   return (i << 24) | ((i & 0xff00) << 8) | ((i >> 8) & 0xff00) | (i >> 24);
 }
--- lib/hash.c
+++ lib/hash.c
@@ -1,5 +1,5 @@
 /* hash - hashing table processing.
-   Copyright (C) 1998, 1999, 2000 Free Software Foundation, Inc.
+   Copyright (C) 1998, 1999, 2000, 2001 Free Software Foundation, Inc.
    Written by Jim Meyering, 1992.
 
    This program is free software; you can redistribute it and/or modify
@@ -263,6 +263,7 @@ hash_get_first (const Hash_table *table)
       return bucket->data;
 
   assert (0);
+  return NULL;
 }
 
 /* Return the user data for the entry following ENTRY, where ENTRY has been
@@ -422,7 +423,7 @@ is_prime (unsigned long candidate)
       divisor++;
     }
 
-  return candidate % divisor != 0;
+  return (candidate % divisor ? true : false);
 }
 
 /* Round a given CANDIDATE number up to the nearest prime, and return that
