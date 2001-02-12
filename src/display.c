78420d256657ff3cdd32b205ab5101900a29af83
Alexandre Duret-Lutz <duret_g@epita.fr>
2001-02-12 08:55:53 +0100

    Important revamp of the LibGGI display driver to not use the `render_visu' intermediate buffer when crossblitting is unneeded, perform double buffering and locking.
    * src/display.c (screen_rv): Move to ...
    [HAVE_LIBSDL] (screen_rv): ... here.
    [HAVE_LIBGGI] (db, current_frame): New variables.
    [HAVE_LIBGGI] (setup_320x200x8_display, setup_320x200xB_display,
    setup_WWWxHHHxB_display, get_directbuffers): New functions.
    [HAVE_LIBGGI] (init_video): Revise, using the above functions.
    [HAVE_LIBGGI] (set_color, set_pal): Don't set paletter on render_visu
    when render_visu == NULL.
    [HAVE_LIBGGI] (vsynchro): Rewrite as a call to vsynchro2.
    [HAVE_LIBGGI] (vsynchro2): Revise to perform double buffering,
    locking, and write to visu or render_visu.

--- src/display.c
+++ src/display.c
@@ -28,17 +28,8 @@
 #include "fastmem.h"
 #include "errors.h"
 
-pixel_t* screen_rv = 0;		/* A pointer to the screen buffer associated
-				   to the render visual. */
-
-/* screen_rv may be a direct pointer to the hardware video buffer, or
-   it may be a pointer to system (mallocated) memory, this depends on
-   the display driver (if the videomode were available or needed to be
-   emulated etc.).  If screen_rv points directly to hardware video
-   it might requires locking. */
-
-int scr_w, scr_h;		/* screen_rv width and height */
-int scr_pitch;			/* screen_rv pitch */
+int scr_w, scr_h;		/* rendering buffer width and height */
+int scr_pitch;			/* rendering buffer pitch */
 
 char video_initialized = 0;	/* has the driver been initialized? */
 
@@ -238,14 +229,38 @@ copy_display (const pixel_t *s, pixel_t *d, unsigned width)
 }
 
 #ifdef HAVE_LIBGGI
+/* LibGGI driver for Heroes.
+
+   Heroes is a 320x200x8bit game so we try to use such a video mode.
+   That's not always possible, so we negociate the closest video mode
+   available.
+
+   When the opened video mode is not 8bit depth, another intermediate
+   visual (`render_visu') is opened an memory to hold the 8bit
+   rendered screens; and this visual is CrossBlited to the main visual
+   by LibGGI.
+
+   Hence, there are two different drawing mode: either direct drawing
+   to the real visual (`visu') if it has a 8bit depth, or drawing to
+   the intermediate visual (`render_visu').  Testing for render_visu
+   == NULL, is enough to know in which mode we are.
+
+   We use double buffering for `visu' whenever possible.
+*/
 
 ggi_visual_t visu = 0;		/* The real display, which receive events. */
-static ggi_visual_t render_visu = 0; /* A 8bit memory-display on which
-					the game is drawn */
+static ggi_visual_t render_visu = NULL; /* A 8bit memory-display on which
+					   the game is drawn, if visu is not
+					   8bit.*/
 
 static ggi_mode vid_mode;
-static char*	display_params = 0;
-static int	full_screen = 0;
+static char *display_params = NULL;
+static int full_screen = 0;
+/* Direct buffer for each frame.
+   We might have db[0] == db[1] if double buffering is not available.*/
+static const ggi_directbuffer *db[2] = { NULL, NULL };
+static int current_frame = 0;	/* used as index to `db' for
+				   double buffering */
 
 void
 set_display_params (const char* str)
@@ -254,16 +269,93 @@ set_display_params (const char* str)
   display_params = xstrdup (str);
 }
 
-void set_full_screen_mode (void)
+void
+set_full_screen_mode (void)
 {
   full_screen = 1;
+}
+
+static bool
+setup_320x200x8_display (void)
+{
+  dmsg (D_VIDEO, "negociate 320x200x8/2 mode");
+  vid_mode.frames = 2;
+  vid_mode.visible.x = scr_w;
+  vid_mode.visible.y = scr_h;
+  vid_mode.virt.x = vid_mode.virt.y = GGI_AUTO;
+  vid_mode.dpp.x = vid_mode.dpp.y = GGI_AUTO;
+  vid_mode.graphtype = GT_8BIT;
+
+  if (ggiCheckMode (visu, &vid_mode) == 0)
+    return ggiSetMode (visu, &vid_mode) == 0;
+  return false;
+}
+
+static bool
+setup_320x200xB_display (void)
+{
+  dmsg (D_VIDEO, "negociate any 320x200xB/2 mode");
+  vid_mode.frames = 2;
+  vid_mode.visible.x = scr_w;
+  vid_mode.visible.y = scr_h;
+  vid_mode.virt.x = vid_mode.virt.y = GGI_AUTO;
+  vid_mode.dpp.x = vid_mode.dpp.y = GGI_AUTO;
+  vid_mode.graphtype = GGI_AUTO;
+
+  if (ggiCheckMode (visu, &vid_mode) == 0)
+    return ggiSetMode (visu, &vid_mode) == 0;
+  return false;
+}
+
+static bool
+setup_WWWxHHHxB_display (void)
+{
+  dmsg (D_VIDEO, "negociate any mode");
+  vid_mode.frames = 2;
+  vid_mode.visible.x = scr_w;
+  vid_mode.visible.y = scr_h;
+  vid_mode.virt.x = vid_mode.virt.y = GGI_AUTO;
+  vid_mode.dpp.x = vid_mode.dpp.y = GGI_AUTO;
+  vid_mode.graphtype = GGI_AUTO;
+
+  ggiCheckMode (visu, &vid_mode);
+  return ggiSetMode (visu, &vid_mode) == 0;
+}
+
+static void
+get_directbuffers (ggi_visual_t *v)
+{
+  int numbuf;
+  int i;
+
+  dmsg (D_VIDEO, "ask for direct-buffers");
+  numbuf = ggiDBGetNumBuffers (v);
+  dmsg (D_VIDEO, "%d direct buffers available", numbuf);
+  for (i = 0; i < numbuf; ++i) {
+    const ggi_directbuffer *d = ggiDBGetBuffer (v, i);
+    if (d == NULL)
+      continue;
+    dmsg (D_VIDEO, "DB#%d: type=%x frame=%d", i, d->type, d->frame);
+    if (!(d->type & GGI_DB_SIMPLE_PLB))
+      continue;
+    if (db[d->frame] == NULL)
+      db[d->frame] = d;
+  }
+
+  if (db[0] != NULL && db[1] == NULL) {
+    dmsg (D_VIDEO, "got only the first direct buffer");
+    db[1] = db[0];
+  } else if (db[0] == NULL && db[1] != NULL) {
+    dmsg (D_VIDEO, "got only the second direct buffer ???");
+    db[0] = db[1];
+  } else if (db[0] == NULL && db[1] == NULL)
+    /* FIXME: ask to send a bug report with a trace */
+    emsg (_("failed to get any direct buffer for this visual"));
 }
 
 void
 init_video (void)
 {
-  const ggi_directbuffer *db;
-
   dmsg (D_VIDEO|D_SYSTEM, "initialize GGI");
   if (ggiInit ()) {
     dmsg (D_VIDEO, "failed");
@@ -278,59 +370,48 @@ init_video (void)
   if (!visu)
     /* TRANS: a `visual' is associated by LibGGI to a display.  You can
        actually call it a `display', this makes no big difference.  */
-    emsg (_("Failed to open visual."));
-
-  dmsg (D_VIDEO, "open display-memory visual");
-  render_visu = ggiOpen ("display-memory", NULL);
-  if (!render_visu)
-    /* TRANS: `display-memory' is the name of a LibGGI driver and
-       make no sense to translate. */
-    emsg (_("Failed to open an internal `display-memory' visual."));
+    emsg (_("failed to open visual."));
 
   scr_w = 320 * stretch;
   scr_h = 200 * stretch;
-  scr_pitch = scr_w;
 
-  vid_mode.frames = 1;
-  vid_mode.visible.x = scr_w;
-  vid_mode.visible.y = scr_h;
-  vid_mode.virt.x = vid_mode.virt.y = GGI_AUTO;
-  vid_mode.dpp.x = vid_mode.dpp.y = GGI_AUTO;
-  vid_mode.graphtype = GT_8BIT;
-
-  dmsg (D_VIDEO, "negociate video mode");
-  if (ggiSetMode (render_visu, &vid_mode) ||
-      /* Try to get a 8bit display */
-      ((ggiCheckGraphMode (visu, GGI_AUTO, GGI_AUTO, scr_w, scr_h, GT_8BIT,
-			   &vid_mode) ||
-	ggiSetGraphMode (visu, GGI_AUTO, GGI_AUTO, scr_w, scr_h, GT_8BIT)) &&
-       /* If unavailable, get any depth available */
-       (ggiCheckGraphMode (visu, GGI_AUTO, GGI_AUTO, scr_w, scr_h, GGI_AUTO,
-			   &vid_mode) ||
-	ggiSetGraphMode (visu, GGI_AUTO, GGI_AUTO, scr_w, scr_h, GGI_AUTO)))) {
-    if (!full_screen ||
-	/* try to get any recommanded video mode */
-	(ggiCheckGraphMode (visu, GGI_AUTO, GGI_AUTO, scr_w, scr_h, GT_8BIT,
-			    &vid_mode) &&
-	 ggiSetMode (visu, &vid_mode))) {
-      emsg (_("Couldn't setup a correct display."));
-    }
+  if (!(setup_320x200x8_display () ||
+	setup_320x200xB_display () ||
+	setup_WWWxHHHxB_display ()))
+    emsg (_("cannot setup a correct display."));
+  /* get info about the mode that was selected */
+  ggiGetMode (visu, &vid_mode);
+  dmsg (D_VIDEO, "video mode is %dx%dx%d/%d",
+	vid_mode.visible.x, vid_mode.visible.y,
+	GT_DEPTH (vid_mode.graphtype), vid_mode.frames);
+  if (vid_mode.virt.x < 320 || vid_mode.virt.y < 200)
+    emsg (_("negociated video mode is too small (width=%d, height=%d)"),
+	  vid_mode.virt.x, vid_mode.virt.y);
+  if (GT_DEPTH (vid_mode.graphtype) == 8) {
+    /* no intermediate buffer required */
+    render_visu = NULL;
+    scr_pitch = vid_mode.virt.x;
+  } else {
+    /* open an intermediate rendefing buffer */
+    dmsg (D_VIDEO, "open display-memory visual");
+    render_visu = ggiOpen ("display-memory", NULL);
+    if (render_visu == NULL)
+      /* TRANS: `display-memory' is the name of a LibGGI driver and
+	 make no sense to translate. */
+      emsg (_("Failed to open an internal `display-memory' visual."));
+    if (ggiSetGraphMode (render_visu, scr_w, scr_h, scr_w, scr_h, GT_8BIT))
+      emsg (_("Failed to set 320x200x8 mode on display-memory"));
+    scr_pitch = scr_w;
   }
-  dmsg (D_VIDEO, "video mode is %dx%dx%d",
-	vid_mode.visible.x, vid_mode.visible.y, GT_DEPTH (vid_mode.graphtype));
 
-  dmsg (D_VIDEO, "ask for a direct-buffer");
-  db = ggiDBGetBuffer (render_visu, 0);
-  if (!db || !(db->type & GGI_DB_SIMPLE_PLB))
-    emsg (_("Can't get correct direct-buffer."));
-
-  screen_rv = db->write;
+  get_directbuffers (render_visu != NULL ? render_visu : visu);
 
   dmsg (D_VIDEO, "set display flags");
   ggiAddFlags (visu, GGIFLAG_ASYNC);
   if (GT_SCHEME (vid_mode.graphtype) == GT_PALETTE)
     ggiSetColorfulPalette (visu);
-  ggiSetColorfulPalette (render_visu);
+  if (render_visu != NULL)
+    ggiSetColorfulPalette (render_visu);
 }
 
 void
@@ -338,15 +419,15 @@ uninit_video (void)
 {
   dmsg (D_MISC, "uninitialize video");
   XFREE0 (display_params);
-  if (render_visu) {
+  if (render_visu != NULL) {
     dmsg (D_VIDEO, "close memory visual");
     ggiClose (render_visu);
-    render_visu = 0;
+    render_visu = NULL;
   }
-  if (visu) {
+  if (visu != NULL) {
     dmsg (D_VIDEO, "close real visual");
     ggiClose (visu);
-    visu = 0;
+    visu = NULL;
   }
   if (video_initialized) {
     dmsg (D_VIDEO, "exit GGI");
@@ -365,7 +446,8 @@ set_color (unsigned char c, unsigned char r, unsigned char g, unsigned char b)
   cmap[c].b = b * 1024;
   dmsg (D_VIDEO, "set color %d=(%d,%d,%d)",c,r,g,b);
   ggiSetPalette (visu, c, 1, cmap);
-  ggiSetPalette (render_visu, c, 1, cmap);
+  if (render_visu != NULL)
+    ggiSetPalette (render_visu, c, 1, cmap);
 }
 
 void
@@ -381,34 +463,67 @@ set_pal (const unsigned char *ptr, int p, int n)
   }
   dmsg (D_VIDEO, "set %d colors", n/3);
   ggiSetPalette (visu, p / 3, n / 3, cmap);
-  ggiSetPalette (render_visu, p / 3, n / 3, cmap);
+  if (render_visu != NULL)
+    ggiSetPalette (render_visu, p / 3, n / 3, cmap);
 }
 
 void
 vsynchro (const pixel_t *s)
 {
-  copy_display (s, screen_rv, 320);
-  ggiCrossBlit (render_visu, 0, 0, scr_w, scr_h, visu,
-		(vid_mode.visible.x - scr_w)/2,
-		(vid_mode.visible.y - scr_h)/2);
-  ggiFlush (visu);
+  vsynchro2 (s, NULL);
 }
 
 void
 vsynchro2 (const pixel_t *s1, const pixel_t *s2)
 {
-  copy_display (s1, screen_rv, 160);
-  copy_display (s2, screen_rv + 160 * stretch, 160);
-  ggiCrossBlit (render_visu, 0, 0, scr_w, scr_h, visu,
-		(vid_mode.visible.x - scr_w)/2,
-		(vid_mode.visible.y - scr_h)/2);
+  pixel_t *dest;
+
+  current_frame ^= 1;
+
+  if (ggiResourceAcquire (db[current_frame]->resource, GGI_ACTYPE_WRITE))
+    wmsg (_("failed to acquire direct-buffer"));
+
+  dest = db[current_frame]->write;
+
+  if (render_visu == NULL)
+    /* center */
+    dest += (vid_mode.visible.x - scr_w)/2 +
+      scr_pitch * (vid_mode.visible.y - scr_h)/2;
+
+  if (s2 == NULL) {
+    copy_display (s1, dest, 320);
+  } else {
+    copy_display (s1, dest, 160);
+    copy_display (s2, dest + 160 * stretch, 160);
+  }
+
+  if (ggiResourceRelease (db[current_frame]->resource))
+    wmsg (_("failed to release direct-buffer"));
+
+  if (render_visu != NULL) {
+    ggiSetWriteFrame(visu, current_frame);
+    ggiCrossBlit (render_visu, 0, 0, scr_w, scr_h, visu,
+		  (vid_mode.visible.x - scr_w)/2,
+		  (vid_mode.visible.y - scr_h)/2);
+  }
+
+  ggiSetDisplayFrame (visu, current_frame);
   ggiFlush (visu);
 }
 
 #endif /* HAVE_LIBGGI */
 #ifdef HAVE_LIBSDL
 
-SDL_Surface* visu = 0;
+pixel_t *screen_rv = 0;		/* A pointer to the screen buffer associated
+				   to the render visual. */
+
+/* screen_rv may be a direct pointer to the hardware video buffer, or
+   it may be a pointer to system (mallocated) memory, this depends on
+   the display driver (if the videomode were available or needed to be
+   emulated etc.).  If screen_rv points directly to hardware video
+   it might requires locking. */
+
+SDL_Surface *visu = 0;
 int visu_options = SDL_HWPALETTE | SDL_DOUBLEBUF;
 char SDL_initialized = 0;
 
