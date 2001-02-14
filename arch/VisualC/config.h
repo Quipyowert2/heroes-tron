4bb10160d2a4fc4c0f318f7e33b882539a1f8988
Alexandre Duret-Lutz <duret_g@epita.fr>
2001-02-14 09:33:44 +0100

    * configure.in: Bump to 0.8e. * NEWS: Update for 0.8e. * po/fr.po: Update.

--- arch/VisualC/config.h
+++ arch/VisualC/config.h
@@ -206,7 +206,7 @@
 #define USE_HEROES_DEBUG 1
 
 /* Version number of package */
-#define VERSION "0.8d"
+#define VERSION "0.8e"
 
 /* whether byteorder is bigendian */
 /* #undef WORDS_BIGENDIAN */
