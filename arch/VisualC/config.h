b776ae5ff5b3346ff2a122856eafacda2a0fa0fc
Alexandre Duret-Lutz <duret_g@epita.fr>
2001-02-20 09:08:26 +0100

    * configure.in: Bump to version 0.8f. * NEWS: Update for 0.8f. * src/Makefile.am: Copyright 2001. * src/keysdef.c: Likewise. * Makefile.am: Likewise. * po/fr.po: Update. * po/nl.po: Update.

--- arch/VisualC/config.h
+++ arch/VisualC/config.h
@@ -206,7 +206,7 @@
 #define USE_HEROES_DEBUG 1
 
 /* Version number of package */
-#define VERSION "0.8e"
+#define VERSION "0.8f"
 
 /* whether byteorder is bigendian */
 /* #undef WORDS_BIGENDIAN */
