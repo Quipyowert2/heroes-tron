f6975ccfaefd6aa8af6996659c472f3f87ada08e
Alexandre Duret-Lutz <duret_g@epita.fr>
2001-07-24 11:49:17 +0200

    * tools/alpha.sh: Reorder and comment sections.

--- tools/alpha.sh
+++ tools/alpha.sh
@@ -10,28 +10,18 @@ eval `autoconf --trace 'AC_INIT:VERSION=$2;PACKAGE=$1'`
 VERSION_=`echo $VERSION | tr . _`
 PACKAGEUP=`echo ${PACKAGE}_ALPHA | tr 'a-z-' 'A-Z_'`
 
+# make distribution
 make distcheck
 make dist-bzip2
 
+# commit it, tag it
 clcommit
 cvs tag ${PACKAGE}_${VERSION_}
 
+# upload it
 scp $PACKAGE-$VERSION.tar.gz $PACKAGE-$VERSION.tar.bz2 shell.sf.net:$HTDOCS_DIR/dl/prereleases
 
-cd ../htdocs
-cvs up
-perl -pi -e "s/(${PACKAGEUP}_RELEASE\()[^,]*,\s*[\d-]+/\${1}${VERSION}, `date -I`/" src/versions.m4
-cat > ChangeLog.head <<EOF
-`date -I`  $WHOCL
-
-	* src/version.m4 (${PACKAGEUP}_RELEASE): Bump to $VERSION.
-
-EOF
-mv ChangeLog ChangeLog.old
-cat ChangeLog.head ChangeLog.old > ChangeLog
-rm ChangeLog.head
-clcommit
-
+# Prepare announce mail.
 cat > mailtxt <<EOF
 $PACKAGE $VERSION has just been uploaded.
 
@@ -46,7 +36,24 @@ Enjoy.
 $WHO
 EOF
 
+# Update web pages.
+(cd ../htdocs
+cvs up
+perl -pi -e "s/(${PACKAGEUP}_RELEASE\()[^,]*,\s*[\d-]+/\${1}${VERSION}, `date -I`/" src/versions.m4
+cat > ChangeLog.head <<EOF
+`date -I`  $WHOCL
+
+	   * src/version.m4 (${PACKAGEUP}_RELEASE): Bump to $VERSION.
+
+EOF
+mv ChangeLog ChangeLog.old
+cat ChangeLog.head ChangeLog.old > ChangeLog
+rm ChangeLog.head
+clcommit
+)
+
 ssh shell.sf.net "cd $HTDOCS_DIR; cvs up; make -k"
 
+# Mail announcement.
 mail -s "$PACKAGE $VERSION uploaded" heroes-discuss@lists.sourceforge.net < mailtxt
 rm mailtxt
